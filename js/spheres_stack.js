b754f588c49d622732c83814fe5ce4c6743fb58c
Abdullah Alfarrarjeh <alfarrar@usc.edu>
2012-12-01 19:00:42 -0800

    changing the color of Cornell Box

--- js/spheres_stack.js
+++ js/spheres_stack.js
@@ -1,31 +1,3 @@
-/*
- WebGL Path Tracing (http://madebyevan.com/webgl-path-tracing/)
- License: MIT License (see below)
-
- Copyright (c) 2010 Evan Wallace
-
- Permission is hereby granted, free of charge, to any person
- obtaining a copy of this software and associated documentation
- files (the "Software"), to deal in the Software without
- restriction, including without limitation the rights to use,
- copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the
- Software is furnished to do so, subject to the following
- conditions:
-
- The above copyright notice and this permission notice shall be
- included in all copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 ////////////////////////////////////////////////////////////////////////////////
 // shader strings
 ////////////////////////////////////////////////////////////////////////////////
@@ -212,8 +184,8 @@ var yellowBlueCornellBox =
 ' else if(hit.x > 0.9999) surfaceColor = vec3(1.0, 0.9, 0.1);'; // yellow
 
 var redGreenCornellBox =
-' if(hit.x < -0.9999) surfaceColor = vec3(1.0, 0.3, 0.1);' + // red
-' else if(hit.x > 0.9999) surfaceColor = vec3(0.3, 1.0, 0.1);'; // green
+' if(hit.x < -0.9999) surfaceColor = vec3(1.0, 0.0, 0.0);' + // red
+' else if(hit.x > 0.9999) surfaceColor = vec3(0.0, 0.0, 1.0);'; // green
 
 function makeShadow(objects) {
   return '' +
